15815e6ed2c1aa3789a657a8608dd3ce244afe28
Ioan Hedeș <ihedes13@gmail.com>
2021-08-03 22:44:56 +0300

    Exports data about the recent subjects in XLS or PDF based on given arg.

--- recent_lessons.py
+++ recent_lessons.py
@@ -1,6 +1,47 @@
 import requests
+import xlsxwriter
+from fpdf import FPDF
 from datetime import datetime
 import sys
+
+
+def create_pdf(data):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.add_font('fireflysung', '', 'fireflysung.ttf', uni=True)
+    pdf.set_font('fireflysung', '', 14)
+
+    pdf.cell(0, 10, 'WaniKani Recently Started Items', 0, 1, 'C')
+    pdf.cell(35, 10, 'Type', 1, 0, 'C')
+    pdf.cell(35, 10, 'Word', 1, 0, 'C')
+    pdf.cell(45, 10, 'Reading', 1, 0, 'C')
+    pdf.cell(0, 10, 'Meaning', 1, 1, 'C')
+    for subject_type, word, reading, meaning in data:
+        pdf.cell(35, 10, subject_type, 1, 0, 'C')
+        pdf.cell(35, 10, word, 1, 0, 'C')
+        pdf.cell(45, 10, reading, 1, 0, 'C')
+        pdf.cell(0, 10, meaning, 1, 1, 'C')
+    pdf.output('subjects.pdf', 'F')
+
+
+def create_spreadsheet(data):
+    workbook = xlsxwriter.Workbook("subjects.xlsx")
+    worksheet = workbook.add_worksheet()
+    row = 0
+    worksheet.write(row, 0, "Type")
+    worksheet.write(row, 1, "Word")
+    worksheet.write(row, 2, "Reading")
+    worksheet.write(row, 3, "Meaning")
+
+    for subject_type, word, reading, meaning in data:
+        row += 1
+        worksheet.write(row, 0, subject_type)
+        worksheet.write(row, 1, word)
+        worksheet.write(row, 2, reading)
+        worksheet.write(row, 3, meaning)
+
+    workbook.close()
+
 
 # Run the script by running recent_lessons <API Bearer Token>
 headers = {"Authorization": "Bearer " + sys.argv[1]}
@@ -25,7 +66,18 @@ rev_list = list(
 
 subjects = list()
 for x in rev_list:
-    subject = requests.get(url='https://api.wanikani.com/v2/subjects/' + str(x['data']['subject_id']), headers=headers).json()
-    subjects.append(subject['data']['characters'])
+    subject = requests.get(
+        url='https://api.wanikani.com/v2/subjects/' + str(x['data']['subject_id']), headers=headers
+    ).json()
+    subjects.append([
+        subject['object'],
+        subject['data']['characters'],
+        subject['data']['readings'][0]['reading'],
+        subject['data']['meanings'][0]['meaning']
+    ])
 
-print(subjects)
+if sys.argv[2] == 'spreadsheet':
+    create_spreadsheet(subjects)
+else:
+    # to generate PDFs, you need to have the fireflysung font ttf file in the same directory
+    create_pdf(subjects)
